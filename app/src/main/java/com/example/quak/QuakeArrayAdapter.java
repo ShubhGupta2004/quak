ef6ddbe0d39e1de92c2b2ad18cefc2dd00d425c2
Shubh Gupta <shubh.gupta2021@vitstudent.ac.in>
2022-06-25 19:27:40 +0530

    1.1

--- app/src/main/java/com/example/quak/QuakeArrayAdapter.java
+++ app/src/main/java/com/example/quak/QuakeArrayAdapter.java
@@ -1,15 +1,16 @@
 package com.example.quak;
 
 import android.app.Activity;
+import android.content.Context;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
 
 public class QuakeArrayAdapter extends ArrayAdapter<earthQuakeData> {
 
-    QuakeArrayAdapter(Activity context,ArrayAdapter<earthQuakeData> arrayAdapter){
-        super(context,0, (List<earthQuakeData>) arrayAdapter);
-        
+    QuakeArrayAdapter(Context context, List<earthQuakeData> arrayAdapter){
+        super(context,0, arrayAdapter);
+
     }
 
 }
