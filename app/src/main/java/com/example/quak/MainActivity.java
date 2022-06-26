89e2da4f535304fd2e287b7ecd51dedbc57daa6b
Shubh Gupta <shubh.gupta2021@vitstudent.ac.in>
2022-06-27 00:21:31 +0530

    1.7 ui modification

--- app/src/main/java/com/example/quak/MainActivity.java
+++ app/src/main/java/com/example/quak/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.quak;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hello..", Toast.LENGTH_SHORT).show();
 
 
     }
